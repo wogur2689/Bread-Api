b87f90a04a6028cc233ebc0294c95d215bfb5ad0
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2025-05-19 00:50:57 +0900

    fix : ProductModule 주입

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { UsersModule } from './web/users/module/users.module';
 import { MenuModule } from './web/menu/module/menu.module';
 import { DatabaseModule } from './database/database.module';
 import { Subscriber } from 'rxjs';
+import { ProductModule } from './web/product/module/product.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, MenuModule, DatabaseModule],
+  imports: [DatabaseModule, UsersModule, MenuModule, ProductModule],
   controllers: [AppController],
   providers: [Subscriber]
 })
