762e179c4c34a7019bc43407cbd8844c74e669b3
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2025-07-01 22:54:47 +0900

    feat : swagger ui 추가

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as session from 'express-session';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -26,6 +27,17 @@ async function bootstrap() {
       },
     })
   )
+
+  //swagger
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('API 문서 제목')
+    .setDescription('API 문서 설명')
+    .setVersion('1.0')
+    .addTag('태그') // 선택사항
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-docs', app, document); // http://localhost:3000/api-docs
 
   await app.listen(process.env.PORT ?? 3001);
 }
